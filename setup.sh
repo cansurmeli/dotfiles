0002f6305750655c5a62f618c397499ab09141b8
Can Sürmeli <cansurmeli@outlook.com>
2020-02-26 10:50:41 +0100

    Reflected the recent directory changes.

--- setup.sh
+++ setup.sh
@@ -13,48 +13,55 @@ read userPlatform
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Contain the dotfiles in their own environment for easy-management
+mkdir ~/.dotfiles
+
 # Common files every platform makes use of
-cp .aliases ~/
-cp .functions ~/
-cp .vimrc ~/
-cp .functions_web_dev ~/
+cp .aliases ~/.dotfiles/
+cp .functions ~/.dotfiles/
+cp .vimrc ~/.dotfiles/
+cp .functions_web_dev ~/.dotfiles/
 
 # Transfer the Vim snippets
 mkdir ~/.vim
 cp -R skeletons ~/.vim
 
+# Other Vim operations
+mkdir ~/.vim/backup
+mkdir ~/.vim/swp
+
 # Linux
 if [ "$userPlatform" -eq 1 ]
 then
 	# Remove the sourcing of unnecessary files
-	sed -i '' -e '/aliases_mac/d' .zshrc
-	sed -i '' -e '/aliases_rpi/d' .zshrc
-	sed -i '' -e '/functions_apple_dev/d' .zshrc
-	sed -i '' -e '/functions_mac/d' .zshrc
-	sed -i '' -e '/functions_rpi/d' .zshrc
+	sed -i '' -e '/.dotfiles/aliases_mac/d' .zshrc
+	sed -i '' -e '/.dotfiles/aliases_rpi/d' .zshrc
+	sed -i '' -e '/.dotfiles/functions_apple_dev/d' .zshrc
+	sed -i '' -e '/.dotfiles/functions_mac/d' .zshrc
+	sed -i '' -e '/.dotfiles/functions_rpi/d' .zshrc
 # macOS
 elif [ "$userPlatform" -eq 2 ]
 then
 	# Additional files needed
-	cp .aliases_mac ~/
-	cp .functions_apple_dev ~/
-	cp .functions_mac ~/
+	cp .aliases_mac ~/.dotfiles/
+	cp .functions_apple_dev ~/.dotfiles/
+	cp .functions_mac ~/.dotfiles/
 
 	# Remove the sourcing of unnecessary files
-	sed -i '' -e '/aliases_rpi/d' .zshrc
-	sed -i '' -e '/functions_rpi/d' .zshrc
+	sed -i '' -e '/.dotfiles/aliases_rpi/d' .zshrc
+	sed -i '' -e '/.dotfiles/functions_rpi/d' .zshrc
 # rPi
 elif [ "$userPlatform" -eq 3 ]
 then
 	# Additional files needed
-	cp .aliases_rpi ~/
-	cp .functions_rpi ~/
+	cp .aliases_rpi ~/.dotfiles/
+	cp .functions_rpi ~/.dotfiles/
 
 	# Remove the sourcing of unnecessary files
-	sed -i '' -e '/aliases_mac/d' .zshrc
-	sed -i '' -e '/functions_apple_dev/d' .zshrc
-	sed -i '' -e '/functions_mac/d' .zshrc
-	sed -i '' -e '/web_dev/d' .zshrc
+	sed -i '' -e '/.dotfiles/aliases_mac/d' .zshrc
+	sed -i '' -e '/.dotfiles/functions_apple_dev/d' .zshrc
+	sed -i '' -e '/.dotfiles/functions_mac/d' .zshrc
+	sed -i '' -e '/.dotfiles/web_dev/d' .zshrc
 else
 	echo "Wrong input! Run the script again."
 	exit 1
--- upgrade.sh
+++ upgrade.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-cp ~/.aliases .
-cp ~/.aliases_mac .
-cp ~/.aliases_rpi .
-cp ~/.functions .
-cp ~/.functions_apple_dev .
-cp ~/.functions_mac .
-cp ~/.functions_rpi .
-cp ~/.functions_web_dev .
-cp ~/.vimrc .
-cp ~/.zshrc .
+cp ~/.dotfiles/.aliases .
+cp ~/.dotfiles/.aliases_mac .
+cp ~/.dotfiles/.aliases_rpi .
+cp ~/.dotfiles/.functions .
+cp ~/.dotfiles/.functions_apple_dev .
+cp ~/.dotfiles/.functions_mac .
+cp ~/.dotfiles/.functions_rpi .
+cp ~/.dotfiles/.functions_web_dev .
+cp ~/.dotfiles/.vimrc .
+cp ~/.dotfiles/.zshrc .
 
+# upgrade the vim skeletons
+cp ~/.vim/skeletons/* skeletons/*
+
+# remove the sensitive files being sourced
 sed -i '' -e '/directories/d' .zshrc
 sed -i '' -e '/confidential/d' .zshrc
