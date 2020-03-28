b8a0d781962fbe22116eaf09293b44f5525e4385
Can Sürmeli <cansurmeli@outlook.com>
2020-03-28 11:04:43 +0100

    Status change.

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Copy everything required
 cp ~/.dotfiles/.aliases .
 cp ~/.dotfiles/.aliases_mac .
 cp ~/.dotfiles/.aliases_rpi .
@@ -8,7 +9,6 @@ cp ~/.dotfiles/.functions_apple_dev .
 cp ~/.dotfiles/.functions_mac .
 cp ~/.dotfiles/.functions_rpi .
 cp ~/.dotfiles/.functions_web_dev .
-cp ~/.vimrc .
 cp ~/.zpreztorc .
 cp ~/.zshrc .
 
