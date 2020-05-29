0286aff2f299c611b191a51aa3d5381031ec1f3f
Can Sürmeli <cansurmeli@outlook.com>
2020-05-29 11:21:10 +0200

    Prepared the remove script. Might need further modifications.

---
+++ remove.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Ask the user for the platform
+echo "Are you sure about removing the dotfiles?"
+echo "1 - yes"
+echo "2 - no"
+
+# Retrieve the user input
+read answer
+
+# Yes
+if [ "$answer" -eq 1 ]
+then
+	# Remove the dotfiles
+	rm -rf ~/.dotfiles
+
+	# Remove the vim folder
+	rm -rf ~/.vim
+# No
+elif [ "$userPlatform" -eq 3 ]
+then
+	echo "Nothing got deleted."
+else
+	echo "Wrong input! Run the script again."
+	exit 1
+fi
